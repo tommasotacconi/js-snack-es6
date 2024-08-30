3f0cbd63b3d2b08d2f5ed56c04acfed2413e6541
tommmasotacconi <tommaso.tacconi@gmail.com>
2024-08-30 17:54:19 +0200

    added snack 4

--- jscript/script_1.js
+++ jscript/script_1.js
@@ -1,4 +1,17 @@
 /* FUNZIONI */
+// funzione per generare tot numeri casuali diversi tra loro e
+// non maggiori di un max
+function getRandomNumbers (max, totalNumbers) {
+  const numbers = [];
+
+  while (numbers.length < totalNumbers) {
+    const randNum = Math.floor(Math.random() * max) + 1;
+    if (!numbers.includes(randNum)) numbers.push(randNum);
+  }
+
+  return numbers;
+}
+
 
 // snack 3
 
@@ -30,8 +43,7 @@ const bikes = [
     weight: 10.6
     ,
   },
-
-]
+];
 
 // FASE DI ELABORAZIONE
 // 1. Con il metodo forEach verifico ogni peso con il successivo al fine di prendere il peso più basso. Imposto una variabile settata sul primo peso dell'array e che tenga conto della bici con peso durante il ciclo con il forEach
@@ -48,3 +60,67 @@ console.log(lighterBike);
 // 5. Recupero il nome della bici più leggera e lo stampo in console
 const lighterBikeName = lighterBike[0].name;
 console.log(lighterBikeName);
+
+
+// snack 4
+
+// FASE DI SET UP
+let streetfootballTeams =
+[
+  {
+    teamName: 'Compagnia dei Celestini',
+    teamPoints: 0,
+    teamFouls: 0,
+  },
+  {
+    teamName: 'Yokkmokk Fiällrävar',
+    teamPoints: 0,
+    teamFouls: 0,
+  },
+  {
+    teamName: 'Slaiv Gallion Braes',
+    teamPoints: 0,
+    teamFouls: 0,
+  },
+  {
+    teamName: 'Manakoko Wallabies',
+    teamPoints: 0,
+    teamFouls: 0,
+  },
+  {
+    teamName: 'Pelorinho Pivetes Bahia',
+    teamPoints: 0,
+    teamFouls: 0,
+  },
+  {
+    teamName: 'Chumatien Shaolin Little Dragons',
+    teamPoints: 0,
+    teamFouls: 0,
+  },
+  {
+    teamName: ' Zaire Red Lions',
+    teamPoints: 0,
+    teamFouls: 0,
+  },
+  {
+    teamName: 'Berliner Aas Devils',
+    teamPoints: 0,
+    teamFouls: 0,
+  },
+];
+
+// FASE DI ELABORAZIONE
+// 1. Aggiungo con il metodo forEach d'array i punti ed i falli delle squadre casualmente, suppondendo che nessuno abbia pià di 20 punti e 10 falli
+for (let team of streetfootballTeams) {
+  team.teamPoints = getRandomNumbers (20, 1)[0];
+  team.teamFouls = getRandomNumbers (10, 1)[0];
+}
+// 2. Stampo in console l'array di oggetti con i numeri casuali inseriti
+console.log(streetfootballTeams);
+// 3. Con map creo un array che contenga solo i nomi e falli subiti
+const newStreetfootballTeams = streetfootballTeams.map(({teamName, teamFouls}) => {
+  const currentTeam = {teamName: teamName, teamFouls: teamFouls};
+return currentTeam ;
+});
+// 4. Stampo in console il nuovo Array
+console.log(newStreetfootballTeams);
